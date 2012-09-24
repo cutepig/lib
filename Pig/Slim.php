a332b2241e937b547e47c23fdb5badaac01540c4
ch <sopo.porsas@gmail.com>
2012-09-24 14:34:19 +0300

    PHP 5.3 version for Pig_Slim

--- Pig/Slim.php
+++ Pig/Slim.php
@@ -7,54 +7,123 @@
 	they do not recognize $this or self because they are not class aware!
 */
 
-class Pig_Slim {
-	private $app = null;
+// PHP_VERSION_ID is available as of PHP 5.2.7, if our 
+// version is lower than that, then emulate it
+if (!defined('PHP_VERSION_ID')) {
+    $version = explode('.', PHP_VERSION);
+    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
+}
 
-	public function __construct($app) {
-		$this->app = $app;
+if(PHP_VERSION_ID >= 50500) {
+	class Pig_Slim {
+		private $app = null;
+
+		public function __construct($app) {
+			$this->app = $app;
+		}
+
+		public function get($route, $classname, $method='get') {
+			$this->app->get($route, function() use ($classname, $method) {
+				$obj = new $classname($this->app);
+				self::handleMethodCall($obj, $method, func_num_args(), func_get_args());
+			});
+		}
+
+		public function post($route, $classname, $method='post') {
+			$this->app->post($route, function() use ($classname, $method) {
+				$obj = new $classname($this->app);
+				self::handleMethodCall($obj, $method, func_num_args(), func_get_args());
+			});
+		}
+
+		public function put($route, $classname, $method='put') {
+			$this->app->put($route, function() use ($classname, $method) {
+				$obj = new $classname($this->app);
+				self::handleMethodCall($obj, $method, func_num_args(), func_get_args());
+			});
+		}
+
+		public function delete($route, $classname, $method='delete') {
+			$this->app->delete($route, function() use ($classname, $method) {
+				$obj = new $classname($this->app);
+				self::handleMethodCall($obj, $method, func_num_args(), func_get_args());
+			});
+		}
+
+		/*
+		 *		PRIVATE METHODS
+		 */
+		static private function handleMethodCall($obj, $method, $numargs, $args) {
+			assert($numargs == count($args));
+			$classname = get_class($obj);
+			switch($numargs) {
+				case 0: $obj->$method(); break;
+				case 1: $obj->$method($args[0]); break;
+				case 2: $obj->$method($args[0], $args[1]); break;
+				case 3: $obj->$method($args[0], $args[1], $args[2]); break;
+				case 4: $obj->$method($args[0], $args[1], $args[2], $args[3]); break;
+				default: debug("Warning: Unsupported number of function arguments for {$classname}->{$method}");
+			}
+		}
 	}
+} else {
+	class Pig_Slim {
+		private $app = null;
 
-	public function get($route, $classname, $method='get') {
-		$this->app->get($route, function() use ($classname, $method) {
-			$obj = new $classname($this->app);
-			self::handleMethodCall($obj, $method, func_num_args(), func_get_args());
-		});
-	}
+		public function __construct($app) {
+			$this->app = $app;
+		}
 
-	public function post($route, $classname, $method='post') {
-		$this->app->post($route, function() use ($classname, $method) {
-			$obj = new $classname($this->app);
-			self::handleMethodCall($obj, $method, func_num_args(), func_get_args());
-		});
-	}
+		public function get($route, $classname, $method='get') {
+			$self = $this;
+			$app = $this->app;
+			$this->app->get($route, function() use ($self, $app, $classname, $method) {
+				$obj = new $classname($app);
+				$self->handleMethodCall($obj, $method, func_num_args(), func_get_args());
+			});
+		}
 
-	public function put($route, $classname, $method='put') {
-		$this->app->put($route, function() use ($classname, $method) {
-			$obj = new $classname($this->app);
-			self::handleMethodCall($obj, $method, func_num_args(), func_get_args());
-		});
-	}
+		public function post($route, $classname, $method='post') {
+			$self = $this;
+			$app = $this->app;
+			$this->app->post($route, function() use ($self, $app, $classname, $method) {
+				$obj = new $classname($app);
+				$self->handleMethodCall($obj, $method, func_num_args(), func_get_args());
+			});
+		}
 
-	public function delete($route, $classname, $method='delete') {
-		$this->app->delete($route, function() use ($classname, $method) {
-			$obj = new $classname($this->app);
-			self::handleMethodCall($obj, $method, func_num_args(), func_get_args());
-		});
-	}
+		public function put($route, $classname, $method='put') {
+			$self = $this;
+			$app = $this->app;
+			$this->app->put($route, function() use ($self, $app, $classname, $method) {
+				$obj = new $classname($app);
+				$self->handleMethodCall($obj, $method, func_num_args(), func_get_args());
+			});
+		}
 
-	/*
-	 *		PRIVATE METHODS
-	 */
-	static private function handleMethodCall($obj, $method, $numargs, $args) {
-		assert($numargs == count($args));
-		$classname = get_class($obj);
-		switch($numargs) {
-			case 0: $obj->$method(); break;
-			case 1: $obj->$method($args[0]); break;
-			case 2: $obj->$method($args[0], $args[1]); break;
-			case 3: $obj->$method($args[0], $args[1], $args[2]); break;
-			case 4: $obj->$method($args[0], $args[1], $args[2], $args[3]); break;
-			default: debug("Warning: Unsupported number of function arguments for {$classname}->{$method}");
+		public function delete($route, $classname, $method='delete') {
+			$self = $this;
+			$app = $this->app;
+			$this->app->delete($route, function() use ($self, $app, $classname, $method) {
+				$obj = new $classname($app);
+				$self->handleMethodCall($obj, $method, func_num_args(), func_get_args());
+			});
+		}
+
+		/*
+		 *		PRIVATE METHODS
+		 */
+		static public function handleMethodCall($obj, $method, $numargs, $args) {
+			assert($numargs == count($args));
+			$classname = get_class($obj);
+			switch($numargs) {
+				case 0: $obj->$method(); break;
+				case 1: $obj->$method($args[0]); break;
+				case 2: $obj->$method($args[0], $args[1]); break;
+				case 3: $obj->$method($args[0], $args[1], $args[2]); break;
+				case 4: $obj->$method($args[0], $args[1], $args[2], $args[3]); break;
+				default: debug("Warning: Unsupported number of function arguments for {$classname}->{$method}");
+			}
 		}
 	}
 }
