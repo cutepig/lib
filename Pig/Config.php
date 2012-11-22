7639503549fa578001729905149de4bd8960c233
ch <sopo.porsas@gmail.com>
2012-11-22 15:26:20 +0200

    New Pig_Config that parses the ini file similar to Zend_Config inheriting sections properly and being able to pick given section and ignore others

--- Pig/Config.php
+++ Pig/Config.php
@@ -1,26 +1,81 @@
 <?php
 
 class Pig_Config {
-	static $config;
+	static $config = null;
 
-	// TODO: Flat-size the config?
-	static function init($path) {
-		if(file_exists($path)) {
-			self::$config = parse_ini_file($path, true);
-			if(self::$config == false)
-				self::$config = array();
-		}
+	static public function init($path, $section=false) {
+		if(file_exists($path))
+			self::initFromArray(parse_ini_file($path, true), $section);
 		else
 			self::$config = array();
 	}
 
-	// TESTING REASONS
-	static function initFromString($str) {
-		self::$config = parse_ini_string($str, true);
+	static public function initFromString($str, $section=false) {
+		self::initFromArray(parse_ini_string($str, true), $section);
 	}
 
-	static function get($key, $default='') {
-		$path = explode('.', $key);
+	// Actual logic in breaking down the sections and choosing the right one
+	static public function initFromArray($arr, $section=false) {
+		if($section) {
+			$newconf = array();
+			foreach($arr as $_section => $content) {
+				if(is_array($content) && !isset($content[0])) {	// Check against key[] = value
+					// Check for inheritance
+					$m = array();
+					if(preg_match('/([\w]+)[\W]*:[\W]*([\w]+)/', $_section, $m)) {
+						$child = $m[1];
+						$parent = $m[2];
+						
+						$newconf[$child] = unserialize(serialize($newconf[$parent]));
+						$_section = $child;
+					}
+					else {
+						// $newconf[$_section] = unserialize(serialize($config[$_section]));
+					}
+
+					//Iterate against the keys and add or merge them to this section
+					foreach($content as $key => $value) {
+						$newconf[$_section][$key] = unserialize(serialize($value));
+					}
+				}
+				else {
+					// Iterate the global keys and add or merge them to this section
+					$newconf[$_section] = unserialize(serialize($content));
+				}
+			}
+
+			// Pick the globals and given section now that we have merged each section
+			self::$config = array();
+			foreach($newconf as $_section => $content) {
+				// If this a section and its a section we want
+				if(is_array($content) && !isset($content[0])) {
+					if($_section == $section) {
+						foreach($content as $key => $value)
+							self::$config[$key] = $value;
+					}
+				}
+				else {
+					// Global key
+					self::$config[$_section] = $content;
+				}
+			}
+		}
+		else {
+			// No section picking, everything goes
+			self::$config = unserialize(serialize($arr));
+		}
+	}
+
+	static public function get($key, $default=false) {
+		if(isset(self::$config[$key]))
+			return self::$config[$key];
+		return $default;
+	}
+
+	static public function get2($key, $default='') {
+		if(!self::$config)
+			return $default;
+		$path = explode('/', $key);
 		$value = self::$config;
 		// Walk
 		foreach($path as $part) {
@@ -40,10 +95,15 @@ class Pig_Config {
 		//	return self::$config[$key];
 		return $value;	// default?
 	}
+
+	static function debug() {
+		print_r(self::$config);
+	}
 }
 
 /*
 // TEST CODE
+// TODO: Update to the new Config way
 $ini = <<<INI
 foo=bar
 [db]
