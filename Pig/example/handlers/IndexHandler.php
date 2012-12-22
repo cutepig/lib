d5fe1d7dcf5a3f993f668517c658d812621c4e06
ch <sopo.porsas@gmail.com>
2012-12-22 17:06:15 +0200

    Empty example project that uses Pig, Slim and Twig

---
+++ Pig/example/handlers/IndexHandler.php
@@ -0,0 +1,11 @@
+<?php
+
+class IndexHandler extends Pig_Slim_BaseHandler {
+	protected function init() {
+		parent::init();
+
+		// Misc stuff..
+	}
+
+	// Implement request handlers
+}
---
+++ Pig/example/web/index.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ *	Project
+ *
+ *	Description: Example for Pig+Slim+Twig environment
+ */
+
+//
+// Setup
+if(!defined('PIG_ENVIRONMENT'))
+	define('PIG_ENVIRONMENT', 'devel');	// devel, qa or prod
+if(!defined('PIG_DEBUG'))
+	define('PIG_DEBUG', true);
+
+//
+// PHP Error reporting
+if(PIG_ENVIRONMENT == 'devel' && PIG_DEBUG == true) {
+	ini_set('error_reporting', E_ALL);
+	ini_set('display_errors', 1);
+}
+
+// Paths
+set_include_path(implode(PATH_SEPARATOR, array(
+	get_include_path(),
+	'../handlers',
+	'../lib',		// local kernel
+	'../../../lib'	// zwamp/vdrive/lib FIXME:
+)));
+
+// Start services
+session_start();
+
+// Autoloader
+require 'Pig/Autoloader.php';
+Pig_Autoloader::init();
+
+// Config test
+Pig_Config::init('../config.ini', PIG_ENVIRONMENT);
+
+//
+// Slim setup
+$app = new Slim\Slim(array(
+	'debug' => Pig_Config::get('slim.debug', PIG_ENVIRONMENT == 'devel'),
+	'mode' => Pig_Config::get('slim.mode', PIG_ENVIRONMENT),
+	// Uncomment view property to disable Twig
+	'view' => new Pig_Slim_TwigView('../views/', array(
+		'debug' => Pig_Config::get('twig.debug', PIG_ENVIRONMENT == 'devel'),
+		// 'cache' => Pig_Config::get('twig.cachedir', ../views/cache')
+	))
+));
+$app->setName(Pig_Config::get('app.name', 'Pig example'));
+
+//
+// Routing helper
+$papp = new Pig_Slim_Autorouter($app);
+
+// Launch the application
+$app->run();
