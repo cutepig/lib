11b90bfc1794b75e58f3e03892e5833fde359ed4
ch <sopo.porsas@gmail.com>
2013-02-15 10:00:50 +0200

    Move json indentation function to Pig_Pig

--- Pig/Slim/BaseHandler.php
+++ Pig/Slim/BaseHandler.php
@@ -46,64 +46,6 @@ class Pig_Slim_BaseHandler {
 		$this->init();
 	}
 
-	/**
-	 * Indents a flat JSON string to make it more human-readable.
-	 *
-	 * @param string $json The original JSON string to process.
-	 *
-	 * @return string Indented version of the original JSON string.
-	 */
-	static protected function indent($json) {
-
-	    $result      = '';
-	    $pos         = 0;
-	    $strLen      = strlen($json);
-	    $indentStr   = '  ';
-	    $newLine     = "\n";
-	    $prevChar    = '';
-	    $outOfQuotes = true;
-
-	    for ($i=0; $i<=$strLen; $i++) {
-
-	        // Grab the next character in the string.
-	        $char = substr($json, $i, 1);
-
-	        // Are we inside a quoted string?
-	        if ($char == '"' && $prevChar != '\\') {
-	            $outOfQuotes = !$outOfQuotes;
-	        
-	        // If this character is the end of an element, 
-	        // output a new line and indent the next line.
-	        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
-	            $result .= $newLine;
-	            $pos --;
-	            for ($j=0; $j<$pos; $j++) {
-	                $result .= $indentStr;
-	            }
-	        }
-	        
-	        // Add the character to the result string.
-	        $result .= $char;
-
-	        // If the last character was the beginning of an element, 
-	        // output a new line and indent the next line.
-	        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-	            $result .= $newLine;
-	            if ($char == '{' || $char == '[') {
-	                $pos ++;
-	            }
-	            
-	            for ($j = 0; $j < $pos; $j++) {
-	                $result .= $indentStr;
-	            }
-	        }
-	        
-	        $prevChar = $char;
-	    }
-
-	    return $result;
-	}
-
 	/*
 	 *		PUBLIC / PROTECTED FUNCTIONS
 	 */
@@ -121,7 +63,7 @@ class Pig_Slim_BaseHandler {
 		$response = $this->app->response();
 		// $response['Content-Type'] = 'application/json';
 		$response->write('<pre>');
-		$response->write(self::indent(json_encode($object/*, JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK*/)));
+		$response->write(Pig_Pig::indentJSON(json_encode($object/*, JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK*/)));
 		$response->write('</pre>');
 	}
 
