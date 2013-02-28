d7843a2a894c84739567a6d86804a5fe1c4531e8
ch <sopo.porsas@gmail.com>
2013-02-28 11:20:07 +0200

    Implement set function to config

--- Pig/Config.php
+++ Pig/Config.php
@@ -72,6 +72,10 @@ class Pig_Config {
 		return $default;
 	}
 
+	static public function set($key, $value) {
+		self::$config[$key] = $value;
+	}
+
 	static public function get2($key, $default='') {
 		if(!self::$config)
 			return $default;
